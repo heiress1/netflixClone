6408de0de141ca79eec181631dd55df853159bf9
Aires Zheng <116032694+heiress1@users.noreply.github.com>
2024-12-25 17:12:58 -0500

    created user schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,36 @@
+//model is used to understand the schema of the user data
+import mongoose from "mongoose"; //import mongoose package
+
+//create a new schema for the user
+const userSchema = new mongoose.Schema({ 
+    username: {
+        type:String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type:String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type:String,
+        required: true,
+    },
+    image: {
+        type:String,
+        default: ""
+    },
+    searchHistory:{
+        type:Array,
+        default: []
+
+    }
+});
+
+//create a model for the user schema
+//create a 'user' model using the userSchema
+//quotation marks need to be single quotes and first letter needs to be uppercase
+//mongo will convert 'User' to lowercase and pluralize it to 'users'
+export const User = mongoose.model('User', userSchema); 
+
