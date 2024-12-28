83696de8298e61373ff81b7fd87050ec263a6fa5
Aires Zheng <116032694+heiress1@users.noreply.github.com>
2024-12-28 04:34:38 -0500

    added comments

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 
+//import the signup, login, and logout functions from the auth.controller file
 import { signup, login, logout } from "../controllers/auth.controller.js";
 
 //create a new router object using the router method from express module
@@ -8,6 +9,9 @@ const router = express.Router();
 
 
 //builds routes
+//sends a post request to the signup endpoint and uses the signup function 
+//when a client (broswer, postman) sends a post request (json data) to the signup function
+//express router calls the matching function wher req and res are auto passed in by express.js
 router.post("/signup", signup);
 
 
