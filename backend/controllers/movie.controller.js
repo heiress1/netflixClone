473774fdd700683beac98e614ac4be74be082626
Aires Zheng <116032694+heiress1@users.noreply.github.com>
2024-12-28 17:52:11 -0500

    create trailer route

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -1,5 +1,6 @@
 import {fetchFromTMDB} from "../services/tmdb.service.js";
 
+
 // this function is called by the express.js router when a post rquest is made to the endpoint
 /**
  * Fetches a trending movie from an external API and sends a random movie from the results as a JSON response.
@@ -13,6 +14,7 @@ import {fetchFromTMDB} from "../services/tmdb.service.js";
  */
 
 export async function getTrendingMovie(req, res) {
+
   try{
     const data = await fetchFromTMDB("https://api.themoviedb.org/3/trending/movie/day?language=en-US");
     const randomMovie = data.results[Math.floor(Math.random() * data.results?.length)];
@@ -23,4 +25,33 @@ export async function getTrendingMovie(req, res) {
 
     res.status(500).json({success:false, message:"Internal Server Error"});
   }
+}
+
+export async function getMovieTrailers(req,res){
+  //{id } is destruturing assingment syntax that allows you to extract properties from objects and assign then to varialbes
+  //id property of the the req.pararms
+  //it extracts the id property and assigsn it to id variable
+  const {id} = req.params;
+
+  try {
+    //this is a dynamic url, the ${id} brings the url to the endpoint of a trailer 
+    //the id represents the movie trailer id
+    const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`);
+    res.json({success:true, trailers:data.results});
+  }catch(error){
+    if (error.message.includes("404")){
+      return res.status(404).send(null)
+    }
+
+    console.error("Error in getMovieTrailers:", error.message);
+
+
+
+    res.status(500).json({success:false, message:"Internal Server Error"});
+
+
+  } 
+
+
+
 }
--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getTrendingMovie } from "../controllers/movie.controller.js";
-
+import { getMovieTrailers } from "../controllers/movie.controller.js";
 //routers are paths to different pages of the website
 //the route will specify how it will respond to a http request
 const router = express.Router();
@@ -9,6 +9,9 @@ const router = express.Router();
 //everything the getTredningmovie function is called, it will return a random movie from the trending moveies list in the tmdb api
 
 router.get("/trending", getTrendingMovie);
+//:id is a route parameter that is used to identify a specific trailer
+//the :id can be any id value
+router.get("/:id/trailers", getMovieTrailers);
 
 
 export default router;
