bd450676bd2cb8b932cbaa36e6417da789413ad9
Aires Zheng <116032694+heiress1@users.noreply.github.com>
2024-12-28 18:06:07 -0500

    created movie detail route

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -55,3 +55,20 @@ export async function getMovieTrailers(req,res){
 
 
 }
+
+export async function getMovieDetails (req, res) {
+  const {id} = req.params;  
+  try {
+    const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}?language=en-US`);  
+    res.status(200).json({success:true, content:data});
+  }catch(error){
+    if (error.message.includes("404")){
+      return res.status(404).send(null)
+    }
+
+    console.error("Error in getMovieDetails:", error.message);
+
+    res.status(500).json({success:false, message:"Internal Server Error"});
+  }
+
+}
--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { getTrendingMovie } from "../controllers/movie.controller.js";
 import { getMovieTrailers } from "../controllers/movie.controller.js";
+import { getMovieDetails } from "../controllers/movie.controller.js";
+import { get } from "mongoose";
 //routers are paths to different pages of the website
 //the route will specify how it will respond to a http request
 const router = express.Router();
@@ -12,6 +14,7 @@ router.get("/trending", getTrendingMovie);
 //:id is a route parameter that is used to identify a specific trailer
 //the :id can be any id value
 router.get("/:id/trailers", getMovieTrailers);
+router.get("/:id/details", getMovieDetails);
 
 
 export default router;
