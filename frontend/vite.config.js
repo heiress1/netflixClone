7da125f90d972b713bbacc73cefd8eaf5ee54785
Aires Zheng <116032694+heiress1@users.noreply.github.com>
2024-12-31 17:24:27 -0500

    added a server proxy to prevent having to type out the localhost when using trying to access a route

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    //everything we use /api, it puts http://localhost:5000 before it
+    //this way we dont need to type out the localhose everytime
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+  },
 })
