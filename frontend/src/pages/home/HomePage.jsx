3e25634c4f2b133990fbec082102cc8a3b9c4e9d
Aires Zheng <116032694+heiress1@users.noreply.github.com>
2025-01-02 15:40:26 -0500

    created dynamic homescreen views using ternary

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import HomeScreen from './HomeScreen.jsx';
 import AuthScreen from './AuthScreen.jsx';
+import { useAuthStore } from '../../store/authUser.js';
 
 const HomePage = () => {
-  const user = false;
+  const {user} = useAuthStore();
   return (
     <div className="">
-      {user ? <HomeScreen /> : <AuthScreen />}
+      {user ? <HomeScreen />  :<AuthScreen />}
     </div>
   )
 }
