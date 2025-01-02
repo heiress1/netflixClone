67c4ab4574a74257ce1d081ec2d02b2c7aef2d9b
Aires Zheng <116032694+heiress1@users.noreply.github.com>
2025-01-02 15:39:28 -0500

    created logout button from homescreen

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useAuthStore } from '../../store/authUser.js';
 
 const HomeScreen = () => {
+
+  const {logout} = useAuthStore();
   return (
     <div>
         <h1>homescreen</h1>
+        <button onClick={logout}>Logout</button>
       
     </div>
   )
