48e5c38a05efea3fae402aac24112b714691e893
Aires Zheng <116032694+heiress1@users.noreply.github.com>
2025-01-03 15:46:58 -0500

    updated the hero image to show different shows on each reload and for movie and show

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+
 import Navbar from '../../components/Navbar'
 import { Link } from 'react-router-dom'
 import { Play, Info } from 'lucide-react'
 import useGetTrendingContent from "../../components/hooks/useGetTrendingContent.jsx";
-import { ORIGINAL_IMG_BASE_URL } from '../../utils/constants.js';
+import { MOVIE_CATEGORIES,TV_CATEGORIES, ORIGINAL_IMG_BASE_URL } from '../../utils/constants.js';
+import Slider from '../../components/Slider.jsx';
+import { useContentStore } from "../../store/content.js";
+import React, { useState, useEffect } from 'react';
+
 
 const HomeScreen = () => {
   const { trendingContent } = useGetTrendingContent();
-  console.log("trendingContent", trendingContent);
+  const {contentType} = useContentStore(); 
+  const [imgLoading, setImgLoading] = useState(true);
 
   if (!trendingContent)
     return (
@@ -20,25 +25,30 @@ const HomeScreen = () => {
 
   return (
     <>
-      <div className="relative h-screen text-white ">
+      <div className="bg-black relative h-screen text-white ">
         <Navbar />
+
+        {/* create shimmer effect while hero image is loading */}
+        {imgLoading && (
+          <div className="absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center shimmer -z-10"/>)}
 
         {/* hero image */}
         <img
-          src={ORIGINAL_IMG_BASE_URL + trendingContent?.backdrop_path}
+          src={`${ORIGINAL_IMG_BASE_URL}${trendingContent?.backdrop_path}`}
           alt="hero image of a movie cover"
-          className="absolute top-0 left-0 w-full h-full object-cover -z-50" />
+          className="absolute top-0 left-0 w-full h-full object-cover -z-0" />
+          onLoad={() => setImgLoading(false)}
         {/* aria-hidden is for accessibility practices */}
 
         {/* gradient purposes */}
-        <div className="absolute top-0 left-0 w-full h-full bg-black/50 -z-50" aria-hidden="true" />
+        <div className="absolute top-0 left-0 w-full h-full bg-black/50 -z-0" aria-hidden="true" />
 
         <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32">
 
           {/* -z-10 in tailwidng sets the elements z-index ti -10 pushing it behind elements with higher z index values 
           this can cause clicks to not pass through your disired element if a z-element is too high */}
           <div
-            className="bg-gradient-to-b from-black via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-10" />
+            className="bg-gradient-to-b from-black via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-50" />
 
           {/* descriptions of the show */}
           <div className="max-w-2xl">
@@ -84,6 +94,17 @@ const HomeScreen = () => {
 
         </div>
       </div>
+
+      {/* movie sliders */}
+
+      <div className="flex flex-col gap-10 bg-black py-10">
+        {contentType === "movie" 
+          ? MOVIE_CATEGORIES.map((category) => <Slider key={category} contentType="movie" category={category} />)   
+           //category={category} is a prop that is passed to the MovieSlider component
+           //the passed prop is taken from the map function 
+          : TV_CATEGORIES.map((category) => <Slider key={category} contentType="tv" category={category} /> )}
+      </div>
+
     </>
 
   )
